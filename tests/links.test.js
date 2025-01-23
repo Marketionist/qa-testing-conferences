057b813929d193fe00feae1b033eecee98585c85
Marketionist <marketionist@gmail.com>
2025-01-23 08:46:09 -0500

    Add links test

---
+++ tests/links.test.js
@@ -0,0 +1,16 @@
+const { checkFile, } = require('img-link-checker');
+
+(async () => {
+    const brokenLinks = await checkFile(
+        './README.md',
+        /* eslint-disable max-len */
+        []
+        /* eslint-enable max-len */
+    );
+
+    // Print a list of broken links and exit the process (will mark the CI/CD build as failed)
+    if (brokenLinks.length > 0) {
+        console.error('Broken links list:', brokenLinks);
+        process.exit(1);
+    }
+})();
